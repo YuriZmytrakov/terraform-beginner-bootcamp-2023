338f346e40e232790eaad4ae3d61dd618d11c8d2
Yuri Zmytrakov <yuri.zmytrakov@gmail.com>
2024-03-18 20:32:47 +0000

    fix deploy static site

--- main.tf
+++ main.tf
@@ -11,4 +11,10 @@ module "terrahouse_aws" {
   source = "./modules/terrahouse_aws"
   user_uuid = var.user_uuid
   bucket_name = var.bucket_name
+  # error_html_filepath = var.error_html_filepath
+  error_html_filepath = "${path.root}/public/error.html"
+
+  # index_html_filepath = var.index_html_filepath
+  index_html_filepath = "${path.root}/public/index.html"
+
 }
