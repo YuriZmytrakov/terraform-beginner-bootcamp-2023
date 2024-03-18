602e09e0e21d25682ad2afe956830f5d1fe9ac7b
Yuri Zmytrakov <yuri.zmytrakov@gmail.com>
2024-03-18 03:08:30 +0000

    add module

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
-resource "aws_s3_bucket" "website_bucket" {
-    bucket = var.bucket_name
-
-    tags = {
-      UserUuid = var.user_uuid
+terraform {
+  cloud {
+    organization = "yurizmytrakov"
+    workspaces {
+      name = "terra-house-1"
+    }
   }
 }
 
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
+}
--- variables.tf
+++ variables.tf
@@ -1,19 +1,7 @@
 variable "user_uuid" {
-  description = "The user uuid"
   type        = string
-
-  validation {
-    condition     = can(regex("^([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})$", var.user_uuid))
-    error_message = "The UUID value is not valid"
-  }
 }
 
 variable "bucket_name" {
-  description = "The name of the S3 bucket"
-  type        = string
-
-  validation {
-    condition     = can(regex("^([a-z0-9-.]+)$", var.bucket_name))
-    error_message = "The bucket name must contain only lowercase letters, numbers, hyphens, and periods."
-  }
+  type = string
 }
