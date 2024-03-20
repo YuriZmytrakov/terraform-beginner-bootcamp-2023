bd2b8c85ab8ff90664a1079f26d080c448acdbf7
Yuri Zmytrakov <yuri.zmytrakov@gmail.com>
2024-03-20 23:54:21 +0000

    add provider

--- main.tf
+++ main.tf
@@ -1,22 +1,41 @@
 terraform {
-  cloud {
-    organization = "yurizmytrakov"
-    workspaces {
-      name = "terra-house-1"
+  required_providers {
+    terratowns ={
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
     }
   }
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "yurizmytrakov"
+
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+  #cloud {
+  #  organization = "yurizmytrakov"
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+}
+provider "terratowns" {
+  endpoint = "http://localhost:4567/api"
+  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
 
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  # error_html_filepath = var.error_html_filepath
-  error_html_filepath = "${path.root}/public/error.html"
+# module "terrahouse_aws" {
+#   source = "./modules/terrahouse_aws"
+#   user_uuid = var.user_uuid
+#   bucket_name = var.bucket_name
+#   # error_html_filepath = var.error_html_filepath
+#   error_html_filepath = "${path.root}/public/error.html"
 
-  # index_html_filepath = var.index_html_filepath
-  index_html_filepath = "${path.root}/public/index.html"
+#   # index_html_filepath = var.index_html_filepath
+#   index_html_filepath = "${path.root}/public/index.html"
 
-  content_version = var.content_version
+#   content_version = var.content_version
   
-}
+# }
--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "bucket_name" {
-  description = "Bucket name for out static hosting"
-  value = module.terrahouse_aws.bucket_name
-}
+# output "bucket_name" {
+#   description = "Bucket name for out static hosting"
+#   value = module.terrahouse_aws.bucket_name
+# }
 
-output "s3_website_endpoint" {
-    description = "S3 static website hosting endpoint"
-    value = module.terrahouse_aws.website_endpoint
-}
+# output "s3_website_endpoint" {
+#     description = "S3 static website hosting endpoint"
+#     value = module.terrahouse_aws.website_endpoint
+# }
 
-output "cloudfront_url" {
-  value = module.terrahouse_aws.cloudfront_url
-  description = "cloudfront url"
-}
+# output "cloudfront_url" {
+#   value = module.terrahouse_aws.cloudfront_url
+#   description = "cloudfront url"
+# }
